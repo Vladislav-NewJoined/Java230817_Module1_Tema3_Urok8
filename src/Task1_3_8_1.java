a0e38b8c027e257317db7daa790cc6c3c8f6386d
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-08-18 13:44:09 +0300

    Initial commit

--- src/Task1_3_8_1.java
+++ src/Task1_3_8_1.java
@@ -1,59 +1,67 @@
 public class Task1_3_8_1 {
     public static void main(String[] args) {
         System.out.println("""
-                Задание:\s
-                1. Реализуйте методы, max(x,y) - для целых, вещественных чисел, и для строк (в
-                   случае строк - возвращает самую длинную)
+            Задание:\s
+            1. Реализуйте методы, max(x,y) - для целых, вещественных чисел, и для строк (в
+               случае строк - возвращает самую длинную)
 
-                Решение:\s""");
+            Решение:\s""");
 
         MaxValue maxValue = new MaxValue();
 
-        //  Вводим исходные значения
-        max(maxValue, 299, 10, 20);  //  для метода max с c переменными Integer
-        max(maxValue, 299.99, 100, 50);  //  для метода max с c переменными Double
-        max(maxValue, "349", "Строка Один", "Строка Два");  //  для метода max с c переменными String
+        System.out.println("Реализуем перегружаемые методы max:  \n");
+
+        max(maxValue,10, 20);  //  для метода max с переменными Integer
+        max(maxValue,100., 50.);  //  для метода max с переменными Double
+        max(maxValue,"Строка Один", "Строка Два");  //  для метода max с переменными String
     }
 
-    //  Реализуем перегруженные методы
-    static void max(MaxValue maxValue, int price, int x, int y) {
+    //  Реализуем перегружаемые методы
+    static void max(MaxValue peremennaya, int x, int y) {
+        peremennaya.maxInt = 0;
         System.out.println("С методом max c переменными Integer: ");
-        if (x > y) {
-            int max = x;
-            System.out.println("Наибольшее значение имеет x = " + max);
-        } else if (y > x) {
-            int max = y;
-            System.out.println("Наибольшее значение имеет y = " + max);
+        System.out.println("Исходные переменные: x = " + x + ", y = " + y);
+        if (x>y) {
+            peremennaya.maxInt = x;
+            System.out.println("Наибольшее значение имеет x = " + peremennaya.maxInt);
+        } else if (y>x) {
+            peremennaya.maxInt = y;
+            System.out.println("Наибольшее значение имеет y = " + peremennaya.maxInt);
         } else {
             System.out.println("Некорректный ввод x и y. Попробуйте еще раз.");
         }
         System.out.println();  //  перенос строки
     }
 
-    static void max(MaxValue balalaika, double price, double x, double y) {
+    static void max(MaxValue peremennaya, double x, double y) {
+        peremennaya.maxDouble = 0;
         System.out.println("С методом max c переменными Double: ");
-        if (x > y) {
-            double max = x;
-            System.out.println("Наибольшее значение имеет x = " + max);
-        } else if (y > x) {
-            double max = y;
-            System.out.println("Наибольшее значение имеет y = " + max);
+        System.out.println("Исходные переменные: x = " + x + ", y = " + y);
+        if (x>y) {
+            peremennaya.maxDouble = x;
+            System.out.println("Наибольшее значение имеет x = " + peremennaya.maxDouble);
+        } else if (y>x) {
+            peremennaya.maxDouble = y;
+            System.out.println("Наибольшее значение имеет y = " + peremennaya.maxDouble);
         } else {
             System.out.println("Некорректный ввод x и y. Попробуйте еще раз.");
         }
         System.out.println();  //  перенос строки
     }
 
-    static void max(MaxValue printer, String price, String firstStr, String secondStr) {
-        System.out.println("С методом max c переменными String: ");
+    static void max(MaxValue peremennaya, String firstStr, String secondStr) {
+        peremennaya.maxString = 0;
+        System.out.println("С методом max c переменными Srting: ");
         int x = firstStr.length();
         int y = secondStr.length();
-        if (x > y) {
-            int max = x;
-            System.out.println("Наибольшую длину имеет строка firstStr, \"" + firstStr + "\", кол-во символов = " + max);
-        } else if (y > x) {
-            int max = y;
-            System.out.println("Наибольшую длину имеет строка secondStr, \"" + secondStr + "\", кол-во символов = " + max);
+
+        System.out.println("Исходные строки: firstStr = \"" + firstStr + "\", secondStr = \"" + secondStr + "\"");
+        if (x>y) {
+            peremennaya.maxString = x;
+            System.out.println("Наибольшую длину имеет строка firstStr, \"" + firstStr + "\", кол-во символов = " + peremennaya.maxString);
+        } else if (y>x) {
+            peremennaya.maxString = y;
+            System.out.println("Наибольшую длину имеет строка secondStr, \"" + secondStr + "\", кол-во символов = " + peremennaya.maxString);
         } else {
             System.out.println("Некорректный ввод строк firstStr и secondStr. Попробуйте еще раз.");
         }
@@ -61,4 +69,7 @@ public class Task1_3_8_1 {
 }
 
 class MaxValue {
+    int maxInt;
+    double maxDouble;
+    int maxString;
 }
